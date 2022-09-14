46503831004abd0f082b349b155a4f78ac83e0db
Samir Aliyev <aliyev7137@gmail.com>
2022-09-14 16:14:07 -0700

    simplifying the code

--- helper.py
+++ helper.py
@@ -2,12 +2,15 @@ def print_ascending(height):
     output = ""
     # go through the height and print appropriate spaces along with the vales
     for i in range(1,height + 1):
-        output += (int(height - i) * ' ') + ( i * '*') + ( int(i-1) * '*')+ '\n'
+        output += print_stars(height, i)
     return output
 
 def print_descending(height):
     output = ""
     # go through the height in reverse order and print the tree appropriately
     for i in range(height, 0, -1):
-        output += (int(height- i) * ' ') +( i * '*') + ( int(i-1) * '*')+ '\n'
+        output += print_stars(height, i)
     return output
+
+def print_stars(height, i):
+    return (int(height- i) * ' ') +( i * '*') + ( int(i-1) * '*')+ '\n'
